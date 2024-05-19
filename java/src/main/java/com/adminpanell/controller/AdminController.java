77c4f016be11aa86540979861bd7e28a451635c5
silver-lui <silver.lui011@gmail.com>
2024-05-20 02:45:58 +0400

    login page and login controller

---
+++ java/src/main/java/com/adminpanell/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.adminpanell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController{
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login_page"
+    }
+}
