39469576c05eee3061c916ae923227c390c72e7e
silver-lui <silver.lui011@gmail.com>
2024-05-20 08:17:42 +0400

    new scroll

--- src/Categories/Main.jsx
+++ src/Categories/Main.jsx
@@ -1,12 +1,12 @@
 import "../Style/Main-Style.css"
-import teming from "../assets/khevsureti-2.png"
+import teming from "../assets/img_1.jpg"
 
 function Main(){
     return(
         <div className="main-wrapper">
             <img src={teming} alt="image" />
             <p className="tags">kiara,kiki</p>
-            <p className="title">fbi open the door</p>
+            <p className="tags">fbi open the door</p>
             <p className="autor">by me</p>
         </div>
     )
--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,36 +1,46 @@
-import "../Style/News-Style.css"
+import "../Style/News-Style.css";
+import img_1 from "../assets/img_2.jpg";
+import img_2 from "../assets/img_3.jpg";
+import img_3 from "../assets/img_4.jpg";
+import img_4 from "../assets/img_5.jpg";
+import img_5 from "../assets/img_6.jpg";
 
 function News() {
     return (
-        <div className="News">
-            <ul className="es">
-                <li>
+        <div className="container">
+            <ul className="news">
+                <li className="ess">
                     <a href="#">
                         <p className="date">May 22, 2028</p>
+                        <img className="news_component" src={img_1} alt="" />
                         <p className="title">იყო და არა იყო რა იყო ერთი გაჭირვებული fრონტენდ დეველოპერი</p>
                     </a>
                 </li>
-                <li>
+                <li className="ess">
                     <a href="#">
                         <p className="date">May 22, 2028</p>
+                        <img className="news_component" src={img_2} alt="" />
                         <p className="title">იყო და არა იყო რა</p>
                     </a>
                 </li>
-                <li>
+                <li className="ess">
                     <a href="#">
                         <p className="date">May 22, 2028</p>
+                        <img className="news_component" src={img_3} alt="" />
                         <p className="title">იყო და არა იყო რა</p>
                     </a>
                 </li>
-                <li>
+                <li className="ess">
                     <a href="#">
                         <p className="date">May 22, 2028</p>
+                        <img className="news_component" src={img_4} alt="" />
                         <p className="title">იყო და არა იყო რა</p>
                     </a>
                 </li>
-                <li>
+                <li className="ess">
                     <a href="#">
                         <p className="date">May 22, 2028</p>
+                        <img className="news_component" src={img_5} alt="" />
                         <p className="title">იყო და არა იყო რა</p>
                     </a>
                 </li>
@@ -39,5 +49,4 @@ function News() {
     );
 }
 
-
-export default News
+export default News;
