c8907d9e0244b2a76d5bdce0579cda7ba017666a
silver-lui <silver.lui011@gmail.com>
2024-05-26 01:18:08 +0400

    footer 3rd

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,13 @@
-import "../../css/footer.css"
+import "../../css/footer.css";
+import img_1 from "../assets/img_2.jpg";
+import img_2 from "../assets/img_3.jpg";
+import img_3 from "../assets/img_4.jpg";
+import img_4 from "../assets/img_5.jpg";
+import img_5 from "../assets/img_6.jpg";
+import img_6 from "../assets/img_1.jpg";
+import linkedin_logo from "../assets/linkedin.png";
+import facebook_logo from "../assets/facebook.png"
+import instagram_logo from "../assets/instagram.png"
 
 function Footer(){
     return(
@@ -14,8 +23,57 @@ function Footer(){
                 </p>
             </div>
 
-            <div className="activities_main">
-                <h6 className="activities_title"></h6>
+            <div className="activites_main">
+                <h6 className="activites_title">Activites</h6>
+                <div className="act-wrapper">
+                    <ul className="activites_main_a">
+                        <li>
+                            <img src={img_1} alt="" />
+                            <p>აქტივობა რომელიც იყო 2018</p>
+                        </li>
+                        <li>
+                            <img src={img_2} alt="" />
+                            <p>აქტივობა რომელიც იყო 2019</p>
+                        </li>
+                        <li>
+                            <img src={img_3} alt="" />
+                            <p>აქტივობა რომელიც იყო 2020</p>
+                        </li>
+                    </ul>
+                    <ul className="activites_main_b">
+                        <li>
+                            <img src={img_4} alt="" />
+                            <p>აქტივობა რომელიც იყო 2021</p>
+                        </li>
+                        <li>
+                            <img src={img_5} alt="" />
+                            <p>აქტივობა რომელიც იყო 2022</p>
+                        </li>
+                        <li>
+                            <img src={img_6} alt="" />
+                            <p>აქტივობა რომელიც იყო 2023</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="contacts_main">
+                    <div className="gmail">
+                        <p>aludagoglichidze@gmail.com</p>
+                    </div>
+                    <div className="phonenumber">
+                        <p>(+995) 599 333 333</p>
+                    </div>
+                    <div className="socials">
+                        <div className="inkdin">
+                            <img src={linkedin_logo} alt="" />
+                        </div>
+                        <div className="facebook">
+                            <img src={facebook_logo} alt="" />
+                        </div>
+                        <div className="instagram">
+                            <img src={instagram_logo} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
