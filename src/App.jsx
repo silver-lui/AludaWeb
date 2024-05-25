e1e15f78c5e2e79cfec9a1b962487b2ea3e41176
silver-lui <silver.lui011@gmail.com>
2024-05-25 18:58:40 +0400

    fotter first

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Category from "./Components/Category";
 import Contact from "./Components/Contact";
 import News from "./Components/News";
 import Content from "./Content";
+import Footer from "./Components/Footer"; 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Category></Category>
       <Content></Content>
       <Contact></Contact>
+      <Footer></Footer>
     </>
   );
 }
