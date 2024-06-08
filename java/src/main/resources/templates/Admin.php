65e94c4229a12f9a5ed45f201cf4d3b2db16b397
silver-lui <silver.lui011@gmail.com>
2024-06-08 14:10:05 +0400

    admin paner

--- java/src/main/resources/templates/Admin.php
+++ java/src/main/resources/templates/Admin.php
@@ -5,17 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>YOU ARE ADMIN !!!</h1>
+<body class="main">
+    <style>
+        .main{
+            background-color: #0080FF;
+        }
+        .tag{
+            text-align: center;
+        }
+        .postArea{
+            height: 400px;
+            width: 800px;
+            align-content: center;
+        }
 
-    <script src="../../../../../php/Upload.php"></script>
-    <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="image">
-        <br/> <br/>
-        <button type="submit" name="submit">submit</button>
-    </form>
+        .cont-wrapper{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+    <div class="cont-wrapper">
+        <h1 class="tag">YOU ARE ADMIN !!!</h1>
 
-    <div>
+        
+        <form method="POST" enctype="multipart/form-data">
+            <script src="Upload.php"></script>
+
+            <input type="file" name="image">
+            <br/> 
+            <textarea name="post" class="postArea"></textarea>
+            <br/>
+            <br/>
+            <ul>
+                <li><input type="checkbox">პროფილი</li>
+                <li><input type="checkbox">აქტივობები</li>
+                <li><input type="checkbox">თემები</li>
+                <li><input type="checkbox">ისტორიები</li>
+            </ul>
+            <br/>
+            <br/>
+            <button type="submit" name="submit">submit</button>
+        </form>
+    </div>
+    <br/>
+    <br/>
+    <div class="cont-wrapper">
         <?php
             $res = mysqli_query($con, "select * from img");
             while($row = mysqli_fetch_assoc($res)){
